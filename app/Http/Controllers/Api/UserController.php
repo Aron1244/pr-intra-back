98212b7f2919e4e62f83860661d4e5ba4b69f0d9
Aron1244 <raycuasa80@gmail.com>
2026-04-06 16:39:18 -0400

    Create user api with Sanctum

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        return UserResource::collection(User::query()->latest()->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request): UserResource
+    {
+        $user = User::create($request->validated());
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user): UserResource
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user): UserResource
+    {
+        $user->update($request->validated());
+
+        return new UserResource($user->refresh());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user): JsonResponse
+    {
+        $user->delete();
+
+        return response()->json(null, 204);
+    }
+}
